1cb7e317952c66c74324516b70d17f4ce0c2bd9a
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-26 09:06:23 -0300

    Tarefa Cebolinha concluída

--- Program.cs
+++ Program.cs
@@ -4,6 +4,5 @@ string fraseDigitada, trocandoRPorL;
 Console.WriteLine("Escleva algo pala mim");
 fraseDigitada = Console.ReadLine()!;
 trocandoRPorL = fraseDigitada
-    .Replace("r","l")
-    .Replace("l","r");
+    .Replace("r","l");
 Console.WriteLine(trocandoRPorL);
